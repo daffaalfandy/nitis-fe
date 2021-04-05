ec4160258130fac9bb0b7a8845aeba0124519c1b
Daffa Alfandy <daffa.alfandy2@gmail.com>
2021-04-05 10:56:22 +0700

    Add watch page

--- env.js
+++ env.js
@@ -4,7 +4,7 @@ module.exports = {
   NODE_ENV: "development",
   SERVER_URI: "http://localhost:3000/api",
   EXPIRED_COOKIES: "1d",
-  YOUTUBE_ID: "",
+  YOUTUBE_ID: "pxcI5g2iUCg",
   ADMIN_URI: "http://localhost:8081",
   TRAILER_YOUTUBE_ID: "U6GtuXd2kdE",
 };
